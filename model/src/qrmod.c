fb4fe0fdd5838b170d52b5e13ea3f89651949b52
Antonio Cuni <anto.cuni@gmail.com>
2017-05-01 01:17:28 +0200

    ouch :(

--- model/src/qrmod.c
+++ model/src/qrmod.c
@@ -208,9 +208,9 @@ void qr_nextstate(qrstate_t *qrstate, double DT)
 	if (a2 < 0) a2 = 0;
 	if (a2 > 1) a2 = 1;
 	if (a3 < 0) a3 = 0;
-	if (a3 > 1) a2 = 1;
+	if (a3 > 1) a3 = 1;
 	if (a4 < 0) a4 = 0;
-	if (a4 > 1) a2 = 1;
+	if (a4 > 1) a4 = 1;
 
 	/* compute rotor speed
 	 */
