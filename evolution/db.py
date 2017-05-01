c2ddf2d176b15259edf601361b6f86ba786bbae2
Antonio Cuni <anto.cuni@gmail.com>
2017-05-01 15:08:59 +0200

    the generation count is kept by the DB

--- evolution/db.py
+++ evolution/db.py
@@ -4,17 +4,25 @@ import cPickle as pickle
 class CreatureDB(object):
 
     def __init__(self, filename):
-        self.conn = sqlite3.connect(filename)
+        self.conn = sqlite3.connect(filename, isolation_level=None) # autocommit
         self.conn.text_factory = str
         self.cur = self.conn.cursor()
         self.cur.execute("""
             CREATE TABLE IF NOT EXISTS creatures (
                 id INTEGER PRIMARY KEY,
-                generation INTEGER,
+                born_at INTEGER,
+                killed_at INTEGER,
                 fitness REAL,
                 pickled TEXT
             )
         """)
+        self.cur.execute("SELECT MAX(born_at) FROM creatures")
+        self.generation = self.cur.fetchone()[0]
+        if self.generation is None:
+            self.generation = 0
+
+    def new_generation(self):
+        self.generation += 1
 
     def save(self, c):
         """
@@ -24,13 +32,14 @@ class CreatureDB(object):
         # create a new row in the DB, to generate an ID
         self.cur.execute("INSERT INTO creatures(id) VALUES(NULL)")
         c.id = self.cur.lastrowid
+        born_at = self.generation
         pickled = pickle.dumps(c)
         # save the updated c
         self.cur.execute("""
             UPDATE creatures
-            SET generation = ?, pickled = ?
+            SET born_at = ?, pickled = ?
             WHERE id = ?
-        """, (c.generation, pickled, c.id))
+        """, (born_at, pickled, c.id))
 
     def load(self, id):
         self.cur.execute("SELECT pickled FROM creatures WHERE id=?", (id,))
@@ -45,10 +54,13 @@ class CreatureDB(object):
             WHERE id = ?
         """, (fitness, c.id))
 
-    def load_stats(self):
+    def load_all(self):
         """
-        Return all the data *except* the creature itself (i.e. only id, generation
-        and fitness)
+        Return all the data *except* the creature itself
         """
-        self.cur.execute("SELECT id, generation, fitness FROM creatures")
+        self.cur.execute("SELECT id, born_at, killed_at, fitness FROM creatures")
         return self.cur.fetchall()
+
+    def count(self):
+        self.cur.execute("SELECT COUNT(*) from creatures")
+        return self.cur.fetchone()[0]
--- testing/test_db.py
+++ testing/test_db.py
@@ -8,17 +8,16 @@ def db():
 
 class FakeCreature(object):
 
-    def __init__(self, generation, data):
+    def __init__(self, data):
         self.id = None
-        self.generation = generation
         self.data = data
 
 
 class TestDB(object):
 
     def test_save_load(self, db):
-        c1 = FakeCreature(42, 'hello')
-        c2 = FakeCreature(43, 'world')
+        c1 = FakeCreature('hello')
+        c2 = FakeCreature('world')
         db.save(c1)
         db.save(c2)
         assert c1.id == 1
@@ -26,23 +25,33 @@ class TestDB(object):
         #
         x1 = db.load(1)
         assert x1.id == 1
-        assert x1.generation == 42
         assert x1.data == 'hello'
         #
         x2 = db.load(2)
         assert x2.id == 2
-        assert x2.generation == 43
         assert x2.data == 'world'
 
     def test_update_fitness(self, db):
-        c1 = FakeCreature(42, 'hello')
-        c2 = FakeCreature(43, 'world')
+        c1 = FakeCreature('hello')
+        c2 = FakeCreature('world')
         db.save(c1)
         db.save(c2)
         db.update_fitness(c1, 10)
         db.update_fitness(c2, 100)
-        rows = db.load_stats()
+        rows = db.load_all()
         assert rows == [
-            (1, 42, 10),
-            (2, 43, 100),
+            (1, 0, None, 10),
+            (2, 0, None, 100),
+        ]
+
+    def test_generation(self, db):
+        c1 = FakeCreature('hello')
+        c2 = FakeCreature('world')
+        db.save(c1)
+        db.new_generation()
+        db.save(c2)
+        rows = db.load_all()
+        assert rows == [
+            (1, 0, None, None), # generation 0
+            (2, 1, None, None), # generation 1
         ]
