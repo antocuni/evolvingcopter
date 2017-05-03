4d81822f7b3c117fb0eb4c5045936a3ba3bfe0aa
Antonio Cuni <anto.cuni@gmail.com>
2017-05-04 00:01:52 +0200

    add the logic to compute the fitness of the creatures

--- evolution/universe.py
+++ evolution/universe.py
@@ -4,8 +4,9 @@ from evolution.environment import Environment
 
 class Universe(object):
 
-    def __init__(self, filename, population=500):
+    def __init__(self, filename, env, population=500):
         self.db = CreatureDB(filename)
+        self.env = env
         self.population = population
         self.alive = set()
         if self.db.count() == 0:
@@ -21,6 +22,27 @@ class Universe(object):
                 c = Creature()
                 self.db.new(c)
                 self.alive.add(c)
+
+    def compute_fitness(self):
+        """
+        Make sure that we computed the fitness of all the alive creatures.
+        """
+        # we check if the fitness has already been computed. This happens in
+        # two cases:
+        #
+        #   1. at each generation, half of the population survived, and thus
+        #      we already know its fitness
+        #
+        #   2. in case we interrupted the simulation in the middle of
+        #      compute_fitness
+        for c in self.alive:
+            fitness = self.db.get_fitness(c)
+            if fitness is None:
+                self.compute_fitness_one(c)
+
+    def compute_fitness_one(self, c):
+        fitness = self.env.run(c)
+        self.db.update_fitness(c, fitness)
 
     def run_one_generation(self):
         self.compute_fitness()
--- testing/test_universe.py
+++ testing/test_universe.py
@@ -1,15 +1,49 @@
 import pytest
+import itertools
 from evolution.universe import Universe
 
 @pytest.fixture
 def filename(tmpdir):
     return str(tmpdir.join('creatures.db'))
 
+class FakeEnv(object):
+
+    def __init__(self):
+        self._fitness = itertools.count(0)
+
+    def run(self, c):
+        return next(self._fitness)
 
 def test_first_generation(filename):
-    uni = Universe(filename, population=10)
+    uni = Universe(filename, env=None, population=10)
     assert len(uni.alive) == 10
     ids = [c.id for c in uni.alive]
     assert sorted(ids) == range(1, 11)
     for c in uni.alive:
         assert uni.db.get_fitness(c) is None
+
+
+def test_compute_fitness(filename):
+    uni = Universe(filename, env=FakeEnv(), population=5)
+    uni.compute_fitness()
+    #
+    for i, c in enumerate(uni.alive):
+        fitness = uni.db.get_fitness(c)
+        assert fitness == i
+
+def test_compute_fitness_partial(filename):
+    uni = Universe(filename, env=FakeEnv(), population=5)
+    creatures = list(uni.alive)
+    creatures.sort(key=lambda c: c.id)
+    c1, c2, c3, c4, c5 = creatures
+    #
+    # manually compute the fitness for c1 and c2
+    uni.db.update_fitness(c1, 10)
+    uni.db.update_fitness(c2, 20)
+    #
+    computed = set()
+    uni.compute_fitness_one = computed.add
+    uni.compute_fitness()
+    assert computed == set([c3, c4, c5])
+
+
