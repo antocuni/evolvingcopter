2a1e2fb28b4b6acc29656b59327b4095983623a1
Antonio Cuni <anto.cuni@gmail.com>
2017-05-06 11:39:36 +0200

    print the matrix when showing the creature

--- ev/show.py
+++ ev/show.py
@@ -1,17 +1,31 @@
 import sys
+import numpy
+import cPickle as pickle
 import ev.pypycompat
 from ev.environment import Environment
 
+import ev.creature
+sys.modules['evolution.creature'] = ev.creature
 
 def main():
-    import cPickle as pickle
+    # don't use scientific notation when printing
+    numpy.set_printoptions(suppress=True)
+
     filename = sys.argv[1]
     with open(filename) as f:
         c = pickle.load(f)
     c.reset()
-    env = Environment(show=True, z1=5, z2=3)
-    #env = Environment(show=True, z1=5, z2=8)
-    print env.run(c)
+
+    print 'Matrix'
+    print c.matrix
+    print
+    print 'Constant'
+    print c.constant
+
+    #env = Environment(show=True, z1=5, z2=3)
+    env = Environment(show=True, z1=5, z2=8)
+    fitness = env.run(c)
+    print 'fitness:', fitness
 
 if __name__ == '__main__':
     main()
